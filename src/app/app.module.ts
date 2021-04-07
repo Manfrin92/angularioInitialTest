3cd59b66550f29690a7ccc9fad702a9d77fba16e
Manfrin92 <manfredd_10@hotmail.com>
2021-04-06 21:27:35 -0300

    feat: custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { ConvertToBrazilianNumber } from './shared/convert-to-brazilian-number.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    ConvertToBrazilianNumber,
+  ],
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
